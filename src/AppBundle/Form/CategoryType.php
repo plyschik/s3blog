e008602d1297d861030051e55a4a43f2f7399b8c
plyschik <palyschik@gmail.com>
2017-07-10 17:59:29 +0200

    CategoryType form added.

---
+++ src/AppBundle/Form/CategoryType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'dashboard.category.form.name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class
+        ]);
+    }
+}
