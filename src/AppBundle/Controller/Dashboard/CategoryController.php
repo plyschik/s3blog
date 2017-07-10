4eefa7fd10b58f851ee020786f15492c2327bea5
plyschik <palyschik@gmail.com>
2017-07-10 18:03:54 +0200

    Category controller added.

---
+++ src/AppBundle/Controller/Dashboard/CategoryController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Dashboard\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class CategoryController extends Controller
+{
+}
