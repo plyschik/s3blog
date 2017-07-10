6e7c62abbb76ac2b90349ecc4c44eb2adc16d002
plyschik <palyschik@gmail.com>
2017-07-10 18:20:17 +0200

    Dashboard categories list added.

--- src/AppBundle/Controller/Dashboard/CategoryController.php
+++ src/AppBundle/Controller/Dashboard/CategoryController.php
@@ -1,10 +1,21 @@
 <?php
 
-namespace AppBundle\Dashboard\Controller;
+namespace AppBundle\Controller\Dashboard;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class CategoryController extends Controller
 {
+    /**
+     * @Route("/dashboard/categories", name="dashboard.categories.list")
+     */
+    public function listAction()
+    {
+        $categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findAll();
+
+        return $this->render('dashboard/categories/list.html.twig', [
+            'categories' => $categories
+        ]);
+    }
 }
