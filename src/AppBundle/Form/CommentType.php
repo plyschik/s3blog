f510355bae7d1c235c1a7e24fdadef2a68787273
plyschik <palyschik@gmail.com>
2017-07-13 12:52:21 +0200

    CommentType added.

---
+++ src/AppBundle/Form/CommentType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('comment', TextareaType::class, [
+                'label' => 'blog.comment.form.comment'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class
+        ]);
+    }
+}
