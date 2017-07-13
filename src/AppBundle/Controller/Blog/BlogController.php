b2bdf0039d368509befaeb45d156221026cbdb1d
plyschik <palyschik@gmail.com>
2017-07-13 12:20:08 +0200

    PanelController added.

--- src/AppBundle/Controller/Blog/BlogController.php
+++ src/AppBundle/Controller/Blog/BlogController.php
@@ -4,7 +4,6 @@ namespace AppBundle\Controller\Blog;
 
 use AppBundle\Entity\Post;
 use Doctrine\ORM\EntityManager;
-use Doctrine\DBAL\Driver\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -79,27 +78,6 @@ class BlogController extends Controller
 
         return $this->render('blog/archive.html.twig', [
             'posts' => $posts
-        ]);
-    }
-
-    public function categoriesPanelAction(EntityManager $entityManager)
-    {
-        return $this->render('blog/panels/categories.html.twig', [
-            'categories' => $entityManager->getRepository('AppBundle:Category')->getBlogCategories()
-        ]);
-    }
-
-    public function tagsPanelAction(EntityManager $entityManager)
-    {
-        return $this->render('blog/panels/tags.html.twig', [
-            'tags' => $entityManager->getRepository('AppBundle:Tag')->findAll()
-        ]);
-    }
-
-    public function archivePanelAction(Connection $connection)
-    {
-        return $this->render('blog/panels/archive.html.twig', [
-            'months' => $connection->fetchAll("SELECT DISTINCT YEAR(p.datetime) AS year, MONTH(p.datetime) AS month, CONCAT(MONTHNAME(p.datetime), ' ', YEAR('p.datetime')) AS monthyear FROM posts p ORDER BY year DESC, month DESC")
         ]);
     }
 }
